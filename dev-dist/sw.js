36b70e3a4c62035a01ca478c5a9734f5989ed1a3
senjay2580 <MetaMighty@outlook.com>
2025-12-21 23:40:54 +0800

    fix

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-2ae722a1'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.b389idfko7"
+    "revision": "0.5pl5qoje1do"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
